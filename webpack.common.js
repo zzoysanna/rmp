a0f143ddee69f19dcafa66c5178cbb97ecc71b16
zzoysanna <zoysanna@gmail.com>
2020-05-10 16:20:44 +0300

    webpack, babel, jest configs

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const BundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports =  {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        index: ['./index.js', './styles/style.less'],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+    },
+    resolve: {
+        extensions: ['.js', '.less'],
+        alias: {
+            '@': path.join(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        // new BundleAnalyzer()
+    ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        port: 3000,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ]
+});
